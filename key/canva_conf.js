6e1e249167d532d047fa857cdee8773fd2140d46
rafkasa4800f-lang <rafkasa4800f@gmail.com>
2026-06-22 10:16:03 +0700

    Create canva_conf.js

---
+++ key/canva_conf.js
@@ -0,0 +1,13 @@
+export const CANVA_SETTINGS = {
+    // Microservice Google Sheets lu:
+    SPREADSHEET_API_URL: "https://script.google.com/macros/s/AKfycbzrDdxtxCKjccciH_rnFILGURTcRW2n3BZ0pKfjH9MQb7i55Bc_gU5amk9kBmQexi5fmw/exec",
+    
+    // KTP Super Admin Canva lu (Ganti bagian ini pakai token mentah lu hasil inspect tadi!):
+    CANVA_AUTH_TOKEN: "zcnvanF6nxN_8Q1g_lGayJhb4MTRDb_...", 
+    
+    // Brand ID (Tim Victor853):
+    BRAND_ID: "BAHMF0b6llQ",
+    
+    // User ID lu:
+    USER_ID: "UAHMFw9vlP4"
+};
