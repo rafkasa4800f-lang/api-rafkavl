8ff8f428a724423c27ff581f3ab5c0ecd22e0297
rafkasa4800f-lang <rafkasa4800f@gmail.com>
2026-06-22 11:14:47 +0700

    Update canva_conf.js

--- key/canva_conf.js
+++ key/canva_conf.js
@@ -3,7 +3,7 @@ export const CANVA_SETTINGS = {
     SPREADSHEET_API_URL: "https://script.google.com/macros/s/AKfycbzrDdxtxCKjccciH_rnFILGURTcRW2n3BZ0pKfjH9MQb7i55Bc_gU5amk9kBmQexi5fmw/exec",
     
     // KTP Super Admin Canva lu (Ganti bagian ini pakai token mentah lu hasil inspect tadi!):
-    CANVA_AUTH_TOKEN: "zcnvanF6nxN_8Q1g_lGayJhb4MTRDb_...", 
+    CANVA_AUTH_TOKEN: "cnvanF6nxN_8Q1g_lGayJhb4MTRDb6RhOfLgr8KQk0OJ_Sm8kN8IlvL592H6HUv3TQbTLytu8Fil_RM8TGMiTlB2FhrQq_sBuVUU3QbjPvT1i7Q_qY2Jmjvm2S0mnNVZN4BFeg3V5PstA1FSRAYf3tYJuwsZJCglubtM4Fyl7eTz4WLkEXYsZiT0SHfNYe5ppUND17oEPOzON1Hs7DgWS_Wfoj3SoslG9DxuSvhW9fQUoRhHY4iVnvlTmTP3RThSDSfQ6ibe41LXZ4-cjMBqtJsc3wEgmPlAMNMV54F-sHBY24ZO4RSqADUPghAonVpxcS8BEHiB7fBzPzUmiOOvbNnrPQOLDPC0ec1d5f0c", 
     
     // Brand ID (Tim Victor853):
     BRAND_ID: "BAHMF0b6llQ",
